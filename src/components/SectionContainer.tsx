a44d361d4d9ee02bb304f5b08d6e374852a31958
chewbacca234 <carine.dupuis.es@gmail.com>
2024-04-11 20:30:02 +0200

    SectionContainer ref debug

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -8,13 +8,16 @@ type Props = {
   children: ReactNode;
 };
 
-export const SectionContainer = forwardRef(
-  ({ title, id, reference, children }: Props): JSX.Element => {
-    return (
-      <div className={styles.container} id={id} ref={reference}>
-        <h2>{title}</h2>
-        {children}
-      </div>
-    );
-  }
-);
+export const SectionContainer = ({
+  title,
+  id,
+  reference,
+  children,
+}: Props): JSX.Element => {
+  return (
+    <div className={styles.container} id={id} ref={reference}>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+};
