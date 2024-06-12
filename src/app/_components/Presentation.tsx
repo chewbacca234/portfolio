a24db65d3c8c11cc674da448f489cb763a4ee64d
chewbacca234 <carine.dupuis.es@gmail.com>
2024-06-12 15:22:25 +0200

    CV link added on profil picture

--- src/app/_components/Presentation.tsx
+++ src/app/_components/Presentation.tsx
@@ -3,34 +3,37 @@ import Image from 'next/image';
 import styles from './Presentation.module.css';
 import {} from '@/hooks';
 import { TechStack } from '.';
+import Link from 'next/link';
 
 export function Presentation() {
   return (
     <div className={styles.container} id="presentation">
       <div className={styles.center}>
         <div className={styles.flipCard}>
-          <div className={styles.flipCardInner}>
-            <div className={styles.flipCardFront}>
-              <Image
-                className={styles.logo}
-                src="/images/logo-CDFullstack-600.png"
-                alt="CD Fullstack Logo"
-                width={300}
-                height={300}
-                priority
-              />
+          <Link href={'/datas/CV Dev fullstack_06_2024.pdf'} target="_blank">
+            <div className={styles.flipCardInner}>
+              <div className={styles.flipCardFront}>
+                <Image
+                  className={styles.logo}
+                  src="/images/logo-CDFullstack-600.png"
+                  alt="CD Fullstack Logo"
+                  width={300}
+                  height={300}
+                  priority
+                />
+              </div>
+              <div className={styles.flipCardBack}>
+                <Image
+                  className={styles.logo}
+                  src="/images/profile_PRO_600.png"
+                  alt="CD Fullstack Logo"
+                  width={300}
+                  height={300}
+                  priority
+                />
+              </div>
             </div>
-            <div className={styles.flipCardBack}>
-              <Image
-                className={styles.logo}
-                src="/images/profile_PRO_600.png"
-                alt="CD Fullstack Logo"
-                width={300}
-                height={300}
-                priority
-              />
-            </div>
-          </div>
+          </Link>
         </div>
       </div>
       <TechStack />
